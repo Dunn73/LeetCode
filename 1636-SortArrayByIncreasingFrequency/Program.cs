619d0e8df8fc5058b62f7ebbb7a1d7b3e0367261
Evan Dunn <dunnev941@gmail.com>
2024-07-23 09:55:16 -0400

    Added 1636

---
+++ 1636-SortArrayByIncreasingFrequency/Program.cs
@@ -0,0 +1,39 @@
+﻿Solution solution = new();
+int[] nums = [1,1,2,2,2,3];
+var output = solution.FrequencySort(nums);
+
+Console.WriteLine("[");
+foreach (var item in output) {
+    Console.Write($"{item}, ");
+}
+Console.WriteLine("]");
+Console.ReadKey();
+public class Solution {
+    public int[] FrequencySort(int[] nums) {
+        // Step 1: Count the frequencies of each number
+        Dictionary<int, int> data = new();
+        
+        foreach (var item in nums) {
+            if (data.ContainsKey(item)) {
+                data[item]++;
+            } else {
+                data.Add(item, 1);
+            }
+        }
+
+        // Step 2: Sort the dictionary by value (frequency) ascending, and by key descending
+        var sortedData = data.OrderBy(pair => pair.Value)
+                             .ThenByDescending(pair => pair.Key);
+
+        // Step 3: Construct the result array
+        List<int> result = new List<int>();
+        
+        foreach (var pair in sortedData) {
+            for (int i = 0; i < pair.Value; i++) {
+                result.Add(pair.Key);
+            }
+        }
+
+        return result.ToArray();
+    }
+}
