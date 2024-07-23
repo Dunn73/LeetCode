de53bde43d4e48a7897960780f37f4eb1d7caf81
Evan Dunn <dunnev941@gmail.com>
2024-07-23 09:27:56 -0400

    Added 1605

---
+++ 1605-FindValidMatrixGivenRowAndColumnSums/Program.cs
@@ -0,0 +1,40 @@
+﻿Solution solution = new();
+
+int[] rowSum = [3,8];
+int[] colSum = [4,7];
+
+var output = solution.RestoreMatrix(rowSum, colSum);
+
+foreach (var row in output) {
+    Console.WriteLine("[");
+    foreach (var item in row ) {
+        Console.WriteLine($"{item}, ");
+    }
+    Console.WriteLine("]");
+
+}
+
+
+Console.ReadKey();
+
+public class Solution {
+    public int[][] RestoreMatrix(int[] rowSum, int[] colSum) {
+        int rows = rowSum.Length;
+        int cols = colSum.Length;
+        int[][] matrix = new int[rows][];
+        for (int i = 0; i < rows; i++) {
+            matrix[i] = new int[cols];
+        }
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                int value = Math.Min(rowSum[i], colSum[j]);
+                matrix[i][j] = value;
+                rowSum[i] -= value;
+                colSum[j] -= value;
+            }
+        }
+
+        return matrix;
+    }
+}
