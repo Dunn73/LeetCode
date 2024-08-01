fd83e45d7e2c64b458fbed821ea490bb9c863732
Evan Dunn <dunnev941@gmail.com>
2024-08-01 18:32:42 -0400

    Added 2678

---
+++ 2678-NumberOfSeniorCitizens/Program.cs
@@ -0,0 +1,23 @@
+﻿Solution solution = new();
+
+string[] details = ["7868190130M7522","5303914400F9211","9273338290F4010"];
+
+Console.WriteLine(solution.CountSeniors(details));
+
+Console.ReadKey();
+public class Solution {
+    public int CountSeniors(string[] details) {
+        int totalSeniors = 0;
+
+        foreach (var element in details) {
+            if (char.GetNumericValue(element[11]) == 6 && char.GetNumericValue(element[12]) > 0){
+                totalSeniors ++;
+            }
+            else if (char.GetNumericValue(element[11]) > 6){
+                totalSeniors ++;
+            }
+        }
+
+        return totalSeniors;
+    }
+}
