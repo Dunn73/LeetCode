7e8fa23a65337a2d62212c4b38eb1a5740b25595
Evan Dunn <dunnev941@gmail.com>
2024-08-03 16:09:35 -0400

    Added 1460

---
+++ 1460-MakeTwoArraysEqualByReversingSubarrays/Program.cs
@@ -0,0 +1,27 @@
+﻿Solution solution = new();
+int[] target = [1,2,3,4];
+int[] arr = [2,4,1,3];
+
+if (solution.CanBeEqual(target, arr) == true) {
+    Console.WriteLine("True");
+}
+else {
+    Console.WriteLine("False");
+}
+Console.ReadKey();
+
+public class Solution {
+    public bool CanBeEqual(int[] target, int[] arr) {
+        Array.Sort(target);
+        Array.Sort(arr);
+        
+        for (int i = 0; i < target.Length; i++){ 
+            if (target[i] != arr[i]) {
+                return false;
+            }
+        }
+
+
+        return true;
+    }
+}
