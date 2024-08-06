bad0026e7bb9e9d2157992a4c87cfb60ed5b8ebe
Evan Dunn <dunnev941@gmail.com>
2024-08-06 18:27:55 -0400

    Added 1508 and 3016

---
+++ 1508-RangeSumOfSortedSubarraySums/Program.cs
@@ -0,0 +1,36 @@
+﻿Solution solution = new();
+int[] nums = [1,2,3,4];
+int n = 4;
+int left = 1;
+int right = 5;
+
+Console.WriteLine(solution.RangeSum(nums, n, left, right));
+
+
+Console.ReadKey();
+public class Solution {
+    public int RangeSum(int[] nums, int n, int left, int right) {
+        const int MOD = 1000000007;
+        List<int> subarraySums = new List<int>();
+
+        // Generate all subarray sums
+        for (int i = 0; i < n; i++) {
+            int currentSum = 0;
+            for (int j = i; j < n; j++) {
+                currentSum += nums[j];
+                subarraySums.Add(currentSum);
+            }
+        }
+
+        // Sort the list of subarray sums
+        subarraySums.Sort();
+
+        // Calculate the sum of the elements from index left to right
+        long sum = 0;
+        for (int k = left - 1; k < right; k++) {
+            sum = (sum + subarraySums[k]) % MOD;
+        }
+
+        return (int)sum;
+    }
+}
---
+++ 3016-MinimumNumberOfPushesToTypeWord2/Program.cs
@@ -0,0 +1,43 @@
+﻿Solution solution = new();
+string word = "abcde";
+
+
+Console.WriteLine(solution.MinimumPushes(word));
+
+Console.ReadKey();
+
+public class Solution {
+    public int MinimumPushes(string word) {
+        int output = 0;
+        Dictionary<char, int> letters = new();
+        foreach (var letter in word) {
+            if (letters.ContainsKey(letter)) {
+                letters[letter] ++;
+            }
+            else {
+                letters.Add(letter, 1);
+            }
+        }
+
+        var sortedLetters = letters.OrderByDescending(kv => kv.Value).ToList();
+
+        //1-8 is 1 key press, 9-16 is 2 key press, 17-24 is 3 key press, 25-26 is 4 key press
+
+        for (int i = 1; i < sortedLetters.Count+1; i++) {
+            if (i < 9) {
+                output += sortedLetters[i-1].Value;
+            }
+            else if (i < 17) {
+                output += sortedLetters[i-1].Value * 2;
+            }
+            else if (i < 25) {
+                output += sortedLetters[i-1].Value * 3;
+            }
+            else {
+                output += sortedLetters[i-1].Value * 4;
+            }
+        }
+
+        return output;
+    }
+}
