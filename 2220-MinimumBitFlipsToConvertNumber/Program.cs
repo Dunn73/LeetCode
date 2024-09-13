08a974eb505ce5e776e785cf60b8872d2a838e09
Evan Dunn <dunnev941@gmail.com>
2024-09-13 18:55:41 -0400

    Added 2220

---
+++ 2220-MinimumBitFlipsToConvertNumber/Program.cs
@@ -0,0 +1,25 @@
+﻿Solution solution = new();
+
+int start = 10;
+int goal = 7;
+
+Console.WriteLine(solution.MinBitFlips(start, goal));
+
+Console.ReadKey();
+public class Solution {
+    public int MinBitFlips(int start, int goal) {
+
+        int numFlips = start ^ goal;
+
+        return FlipHelper(numFlips);
+    }
+
+    public int FlipHelper(int n) {
+        int count = 0;
+        while (n > 0) {
+            count += n & 1;
+            n >>= 1;
+        }
+        return count;
+    }
+}
